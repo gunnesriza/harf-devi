ecf816956dec01393f6845c5c847a93af2d665fa
gunnesriza <gunes1234riza@gmail.com>
2025-01-09 22:56:45 +0300

    Add files via upload

---
+++ harf ödevi/gunes/Main.java
@@ -0,0 +1,9 @@
+package com.gurkay;
+
+public class Main {
+    public static void main(String[] args) {
+        //�smi yazdıralım
+        AsciiArtPrinter asciiArtPrinter = new AsciiArtPrinter();
+        asciiArtPrinter.yazdir("GURKAY");
+    }
+}
