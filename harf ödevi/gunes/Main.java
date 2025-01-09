08663c81b14cca76be1653b323ebdf51806b490f
gunnesriza <gunes1234riza@gmail.com>
2025-01-09 22:57:29 +0300

    Delete harf ödevi directory

--- harf ödevi/gunes/Main.java
+++
@@ -1,9 +0,0 @@
-package com.gurkay;
-
-public class Main {
-    public static void main(String[] args) {
-        //�smi yazdıralım
-        AsciiArtPrinter asciiArtPrinter = new AsciiArtPrinter();
-        asciiArtPrinter.yazdir("GURKAY");
-    }
-}
